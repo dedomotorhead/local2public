1c953f637a2a4dc75fe78db126ba8433b6857de3
Maros Vrecnik <vrecnik@eset.sk>
2017-03-10 11:10:26 +0100

    temporary commit

--- run_proxy.py
+++ run_proxy.py
@@ -6,7 +6,14 @@ import socket
 import sys
 import select
 from threading import Thread
-
+#---------------------------------------------------------------------------------------------
+# handle CTRL + C
+import signal
+import sys
+def signal_handler(signal, frame):
+  print('You pressed Ctrl+C!')
+signal.signal(signal.SIGINT, signal_handler)
+#---------------------------------------------------------------------------------------------
 HOST = ''   # Symbolic name, meaning all available interfaces
 PORT = 8888 # Arbitrary non-privileged port
 
@@ -29,7 +36,7 @@ server_socket.listen(10)
 sock_2_local_server = []
 # list of socket from remote clients
 sock_from_client = []
-
+#---------------------------------------------------------------------------------------------
 # communicating thread
 do_comunicate = True
 
@@ -51,6 +58,11 @@ def communicate():
     except select.error:
       break
 
+    for s in inputready:
+      data = s.recv(4096)
+      if not data:
+        print('Connection is closed')
+        
 
   '''if s_acc == conn:
     data = conn.recv(4096)
@@ -74,22 +86,32 @@ def communicate():
       conn.sendall(data)
       print('send data to client')
 '''
-
+#---------------------------------------------------------------------------------------------
 # run communicating thread
 t1 = Thread(target=communicate, args=[])
 t1.start()
 
 #now keep talking with the client
 while True:
+
+  try:
   
-  print ('Socket now listening')
-  sock, addr = server_socket.accept()
-  print ('Connected with ' + addr[0] + ':' + str(addr[1]))
-  print('socket from client: ' + str(sock.fileno()))
-  sock_from_client.append(sock)
-  sock_2_local_server.append(socket.create_connection(('localhost', 3000)))
-      
-  print('Waiting for next client...')
-  
+    print ('Socket now listening')
+    sock, addr = server_socket.accept()
+    print ('Connected with ' + addr[0] + ':' + str(addr[1]))
+    print('socket from client: ' + str(sock.fileno()))
+    sock_from_client.append(sock)
+    sock_2_local_server.append(socket.create_connection(('localhost', 3000)))
+    print('Waiting for next client...')
+
+  except KeyboardInterrupt:
+    do_comunicate = False
+    t1.join()
+    for s in sock_2_local_server :
+      s.close()
+
+    for s in sock_from_client :
+      s.close()
+#---------------------------------------------------------------------------------------------
 # finito, the server is going down
 server_socket.close()
